1363dd91d0ca691c2caeddabbdb7a96e9edbf18b
Lyn Santiago Enriquez <lysa1900@student.miun.se>
2021-04-21 17:00:32 +0200

    Remade the whole menu for better structure (Trello ID 2.5 In Progress)
    Co-authored-by: Pilvii <Pilvii@users.noreply.github.com>

--- components/header.inc.php
+++ components/header.inc.php
@@ -14,8 +14,24 @@
 
 <body>
     <header>
-        <button class="hamburger-icon" onclick="toggleMenu()">&#9776;</button>
+        <div class="menu">
+            <button class="hamburger-icon" onclick="toggleMenu()">&#9776;</button>
+            <div class="menu-container">
+                <ul class="navlist">
+                    <li class="navlist-item" onclick="toggleSubmenu(this)"><a href="#">Vad är ett API?<i class="fas fa-angle-down drop-down"></i></a>
+                        <ul class="submenu d-none">
+                            <li><a href="">Öppna data</a></li>
+                            <li><a href="">Öppna data</a></li>
+                            <li><a href="">Öppna data</a></li>
+                            <li><a href="">Öppna data</a></li>
+                        </ul>
+                    </li>        
+                    <li class="navlist-item"><a href="#">Hur man använder ett API?</a></li>
+                    <li class="navlist-item"><a href="#">Varför ska man använda ett API??</a></li>
+                    <li class="navlist-item"><a href="#">Kodexempel</a></li>
+                    <li class="navlist-item"><a href="#">Quiz</a></li>
+                </ul>
+            </div>
+        </div>
         <a class="logo" href="index.php">APIskolan</a>
     </header>
-
-
--- styles/main.js
+++ styles/main.js
@@ -1,8 +1,3 @@
-function toggleMenu(){
-    let menu = document.querySelector('.navlist');
-    toggleDisplayNone(menu);
-}
-
 function toggleSubmenu(ev){
     let submenu = ev.lastElementChild;
     toggleDisplayNone(submenu);
@@ -15,4 +10,26 @@ function toggleDisplayNone(element){
     else{
         element.classList.add('d-none');
     }
+}
+
+function toggleMenu(){
+    let medium = window.matchMedia("(min-width: 768px)");
+    
+    if(medium.matches){
+        changeMenuWidth("40vw")
+    }
+    else{
+        changeMenuWidth("75vw")
+    }
+}
+
+function changeMenuWidth(newWidth){
+    let menu = document.querySelector('.menu-container');
+    if(menu.style.width == newWidth)
+        {
+            menu.style.width = "0";
+        }
+        else{
+            menu.style.width = newWidth;
+        }
 }
