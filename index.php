30c06b6dfdd0a320261951f0051aad5f554083a5
Pilvii <pilviborjesson@gmail.com>
2021-04-22 09:19:00 +0200

    Center logo and footer over main content. Styling main content. (Trello ID 1 In Progress)
    Co-authored-by: Lyn Enriquez <lynsan@users.noreply.github.com>

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php require('components/header.inc.php')?>
 
     <div class="main-content">
-        <h1>Välkommen till APIskolan!</h1>
-        <p>Har du hört talas om API men vet inte riktigt vad det är för något? Eller du vill kanske lära en kompis hur API fungerar? I den här webbplatsen kan du hitta de grundläggande information du behöver för att lära om API.</p>
-        <button type="button" class="btn btn-primary">Kom igång!</button>
+        <section>
+            <h1>Välkommen till APIskolan!</h1>
+            <p>Har du hört talas om API men vet inte riktigt vad det är för något? Eller du vill kanske lära en kompis hur API fungerar? I den här webbplatsen kan du hitta de grundläggande information du behöver för att lära om API.</p>
+            <button type="button" class="btn btn-primary">Kom igång!</button>
+        </section>
     </div>
     
 <?php include('components/footer.inc.php')?>
